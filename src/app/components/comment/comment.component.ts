691671e5c662f8eeac9c41123874cf5a4eba99b9
Courtney Carson <cmacarson@gmail.com>
2023-10-14 18:32:18 -0700

    comments form

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommentFormComponent } from '../comment-form/comment-form.component';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CommentFormComponent],
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss'],
 })
